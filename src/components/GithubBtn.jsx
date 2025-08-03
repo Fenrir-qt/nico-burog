10ff7b7967fbc82edd1dca3966226213056431f8
Fenrir-qt <mickburog87@gmail.com>
2025-08-03 19:24:59 +0800

    Removed bottom margin of github icon in github button component

--- src/components/GithubBtn.jsx
+++ src/components/GithubBtn.jsx
@@ -4,7 +4,7 @@ function GitBtn() {
     <>
       <button className="flex items-center gap-1 w-fit h-8 pl-2 pr-2 bg-black rounded-md text-white text-sm mt-2 cursor-pointer hover:bg-neutral-800 transform tranFation duration-200">
         GitHub
-        <FaGithub className="mb-0.5"/>
+        <FaGithub />
       </button>
     </>
   );
